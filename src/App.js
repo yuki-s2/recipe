34b6a1ace733006ac81038e744126fb14f10d999
Yuki Honda <dahlia.15s2@gmail.com>
2024-01-30 23:01:54 +0900

    uuid

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import { useState, useRef } from 'react';
 import TodoList from './TodoList';
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const [todos, setTodos] = useState([
-    { id: 1, name:"todo1", completed: false },
-]);
+    { id: 1, name: "todo1", completed: false },
+  ]);
 
-const todoNameRef = useRef();
+  const todoNameRef = useRef();
 
-const handleAddTodo = () => {
-const name = todoNameRef.current.value;
-setTodos((prevTodos) => {
-  return [...prevTodos, { id: "1", name: name, completed: false }];
-});
-todoNameRef.current.value = null;
-};
+  const handleAddTodo = () => {
+    const name = todoNameRef.current.value;
+    setTodos((prevTodos) => {
+      return [...prevTodos, { id: uuidv4(), name: name, completed: false }];
+    });
+    todoNameRef.current.value = null;
+  };
   return (
     <>
-    <TodoList todos={todos} />
-    <input type="text" ref={todoNameRef} />
-    <button onClick={handleAddTodo}>タスクを追加</button>
-    <button>完了したタスクの削除</button>
-    <div>残りのタスク:0</div>
+      <TodoList todos={todos} />
+      <input type="text" ref={todoNameRef} />
+      <button onClick={handleAddTodo}>タスクを追加</button>
+      <button>完了したタスクの削除</button>
+      <div>残りのタスク:0</div>
     </>
   );
 }
