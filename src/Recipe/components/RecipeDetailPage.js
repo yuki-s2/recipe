86fee93f7290b0659c7a8446de201fbdca5312f4
Yuki Honda <dahlia.15s2@gmail.com>
2024-04-24 20:42:35 +0900

    input css追加

--- src/Recipe/components/RecipeDetailPage.js
+++ src/Recipe/components/RecipeDetailPage.js
@@ -21,9 +21,19 @@ export const RecipeDetailPage = ({ recipes }) => {
           <li className='recipeDetail_editItem'>削除</li>
         </ul>
         <div className="recipeDetail_img">
-          <img src="" alt="" />
+{/* <svg width="0" height="0" style="position: absolute; top: 0; left: 0;">
+  <clipPath id="clip01">
+  <path d="M390 196.5V326C390 338.703 379.703 349 367 349H26C13.2975 349 3 338.703 3 326V196.5C3 89.6329 89.6329 3 196.5 3C303.367 3 390 89.6329 390 196.5Z" fill="#D9D9D9" stroke="#D5D5D5" stroke-width="6"/>
+    </clipPath>
+</svg>
+<img src="" alt="" />
+ 
+<svg width="300" height="300" viewBox="0 0 300 300">
+  <img src="../images/aaa.jpeg" width="100%" height="100%" preserveAspectRatio="xMidYMid slice" clip-path="url(#clip01)"/>
+</svg> */}
         </div>
         <h3 className='recipeDetail_name'>{recipe.name}</h3>
+        <img src="./images/aaa.jpeg" alt="" />
         <RecipeIngredients recipe={recipe} />
         <div className="recipeDetail_detailsContainer">
           <h4 className='recipeDetail_title'>作り方</h4>
