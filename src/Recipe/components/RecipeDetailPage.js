61ac04780f3d2f6a955eae9e03ca5dffb61a1287
Yuki Honda <dahlia.15s2@gmail.com>
2024-05-01 15:30:26 +0900

    クリップパスで画像表示

--- src/Recipe/components/RecipeDetailPage.js
+++ src/Recipe/components/RecipeDetailPage.js
@@ -15,43 +15,53 @@ export const RecipeDetailPage = ({ recipes }) => {
   return (
     <div className='recipeDetail_body'>
       <div className="inner">
-        <h2 className='page_ttl'>今日の献立</h2>
+      <h3 className='recipeDetail_name'>{recipe.name}</h3>
         <ul className="recipeDetail_edit">
           <li className='recipeDetail_editItem'>編集</li>
           <li className='recipeDetail_editItem'>削除</li>
         </ul>
-        <div className="recipeDetail_img">
-{/* <svg width="0" height="0" style="position: absolute; top: 0; left: 0;">
-  <clipPath id="clip01">
-  <path d="M390 196.5V326C390 338.703 379.703 349 367 349H26C13.2975 349 3 338.703 3 326V196.5C3 89.6329 89.6329 3 196.5 3C303.367 3 390 89.6329 390 196.5Z" fill="#D9D9D9" stroke="#D5D5D5" stroke-width="6"/>
-    </clipPath>
-</svg>
-<img src="" alt="" />
- 
-<svg width="300" height="300" viewBox="0 0 300 300">
-  <img src="../images/aaa.jpeg" width="100%" height="100%" preserveAspectRatio="xMidYMid slice" clip-path="url(#clip01)"/>
-</svg> */}
-        </div>
-        <h3 className='recipeDetail_name'>{recipe.name}</h3>
-        <img src="./images/aaa.jpeg" alt="" />
+
+      <div class="svgContent_main">
+        <svg width="0" height="0" viewBox="0 0 393 352">
+          <clipPath id="clip01">
+            <path d="M390 196.5V326C390 338.703 379.703 349 367 349H26C13.2975 349 3 338.703 3 326V196.5C3 89.6329 89.6329 3 196.5 3C303.367 3 390 89.6329 390 196.5Z" />
+          </clipPath>
+        </svg>
+      </div>
+      <div className="svgContent_mainImg">
+      <svg width="70%" height="70%" viewBox="0 0 393 352">
+        <image href="/images/img_2.JPG" width="100%" height="100%" preserveAspectRatio="xMidYMid slice" clip-path="url(#clip01)"/>
+      </svg>
+      </div>
+
+
+      <div className="svgContent_subImg">
+        <img src="/images/img_2.JPG" alt="" />
+        <img src="/images/img_2.JPG" alt="" />
+        <img src="/images/img_2.JPG" alt="" />
+        <img src="/images/img_2.JPG" alt="" />
+        <img src="/images/img_2.JPG" alt="" />
+      </div>
+
         <RecipeIngredients recipe={recipe} />
         <div className="recipeDetail_detailsContainer">
           <h4 className='recipeDetail_title'>作り方</h4>
           <p className='recipeDetail_detailsText'>{recipe.details}</p>
         </div>
-        <div className="btn_container">
+
+      <div className="btn_container">
         <Link to="/SelectedRecipes">
           <div className='btn_link'>選択れたレシピ一覧へ</div>
         </Link>
         <Link to="/RecipeListPage">
           <div className='btn_link'>追加されたレシピ一覧へ</div>
         </Link>
-        </div>
-        <div className="btn_container">
+      </div>
+      <div className="btn_container">
         <Link to="/">
           <div className='btn_link'>リストに戻る</div>
         </Link>
-        </div>
+      </div>
       </div>
     </div>
   );
