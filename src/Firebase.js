c52014d3a488ca2ebd1f22fe86646626ada931c2
Yuki Honda <dahlia.15s2@gmail.com>
2024-05-05 21:18:48 +0900

    firebaseインストール

---
+++ src/Firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+//ファイヤーベースの初期化
+const firebaseConfig = {
+    apiKey: "AIzaSyCwl10MSlI72Q97Duo4CU1pT6iw7JZbGGo",
+    authDomain: "my-recipe-b7757.firebaseapp.com",
+    projectId: "my-recipe-b7757",
+    storageBucket: "my-recipe-b7757.appspot.com",
+    messagingSenderId: "401708231650",
+    appId: "1:401708231650:web:ad349121ae9721f53f67bb"
+  };
+
+  const app = initializeApp(firebaseConfig);
