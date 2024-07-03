ab11a8a234f9b8e79d2fecd1db55f0f24db71b86
Yuki Honda <dahlia.15s2@gmail.com>
2024-07-03 23:31:21 +0900

    作り方画像アップロードUI

--- src/Recipe/components/RecipeInputPage.js
+++ src/Recipe/components/RecipeInputPage.js
@@ -196,6 +196,7 @@ export const RecipeInputPage = ({ posts }) => {
           </>
         ) : (
           <input
+            className='input'
             type='file'
             accept='.png, .jpg, .jpeg'
             onChange={handleFileUploadToFirebase}
@@ -210,56 +211,68 @@ export const RecipeInputPage = ({ posts }) => {
             </div>
           </div>
           <div className="recipeInput_contents">
-          <div className="recipeInput_container">
-          <h2 className='page_ttl'>新しいレシピを追加する</h2>
-          <form className='recipeInput_form' onSubmit={handleSubmit}>
-            <div>
-              <div className='recipeInput_item'>
-                <div className="recipeInput_title">レシピの名前</div>
-                <input type="text" onChange={(e) => setNewRecipeName(e.target.value)} value={newRecipeName} />
-              </div>
-              <div className="recipeInput_item recipeInput_ingredient">
-                <div className="recipeInput_title">材料</div>
-                {newIngredients.map((ingredient, index) => (
-                  <input
-                    key={index}
-                    type="text"
-                    value={ingredient}
-                    onChange={(event) => handleAdditionalInfoChange(index, event)}
-                  />
-                ))}
-                <button className='button_additionBtn' type="button" onClick={handleAddIngredientField}>
-                  追加する
-                </button>
-              </div>
-              <div className="recipeInput_item recipeInput_ingredient">
-                <div className="recipeInput_title">作り方の画像</div>
-                {editedRecipe.images_detailUrl.map((images_detailUrl, index) => (
-                  <div key={index}>
-                    <img src={images_detailUrl} alt="Recipe Detail" style={{ width: '100px', height: '100px' }} />
-                    <button type="button" onClick={() => handleRemoveImage(index)}>画像を削除</button>
+            <div className="recipeInput_container">
+              <h2 className='page_ttl'>新しいレシピを追加する</h2>
+              <form className='recipeInput_form' onSubmit={handleSubmit}>
+                <div>
+                  <div className='recipeInput_item'>
+                    <div className="recipeInput_title">レシピの名前</div>
+                    <input className='input' type="text" onChange={(e) => setNewRecipeName(e.target.value)} value={newRecipeName} />
                   </div>
-                ))}
-                <input
-                  type='file'
-                  multiple
-                  accept='.png, .jpg, .jpeg'
-                  onChange={handleFileSelection}
-                />
-                {loadingDetailImgs ? (
-                  <p>詳細画像をアップロード中...</p>
-                ) : (
-                  <p>画像をアップロード</p>
-                )}
-              </div>
-              <div className="recipeInput_item">
-                <h3 className="recipeInput_title">作り方</h3>
-                <input type="text" onChange={(e) => setNewDetail(e.target.value)} value={newDetail} />
-              </div>
+                  <div className="recipeInput_item recipeInput_ingredient">
+                    <div className="recipeInput_title">材料</div>
+                    {newIngredients.map((ingredient, index) => (
+                      <input
+                        className='input'
+                        key={index}
+                        type="text"
+                        value={ingredient}
+                        onChange={(event) => handleAdditionalInfoChange(index, event)}
+                      />
+                    ))}
+                    <button className='button_additionBtn' type="button" onClick={handleAddIngredientField}>
+                      追加する
+                    </button>
+                  </div>
+                  <div className="recipeInput_item">
+                    <div className="recipeInput_title">作り方の画像</div>
+                    {editedRecipe.images_detailUrl.map((images_detailUrl, index) => (
+                      <div key={index}>
+                        <img src={images_detailUrl} alt="Recipe Detail" style={{ width: '100px', height: '100px' }} />
+                        <button type="button" onClick={() => handleRemoveImage(index)}>画像を削除</button>
+                      </div>
+                    ))}
+                    <div className="recipeInput_wrap">
+                      <div className="recipeInput_head">
+                        <div className="add">add</div>
+                      </div>
+                      <div className="recipeInput_contents">
+                      <div className="recipeInput_menu">
+                          <input
+                            className='input_img'
+                            type='file'
+                            multiple
+                            accept='.png, .jpg, .jpeg'
+                            onChange={handleFileSelection}
+                          />
+                          <button><img className="recipeInput_delete" src="" alt="削除" /></button>
+                        </div>
+                        {loadingDetailImgs ? (
+                          <p>詳細画像をアップロード中...</p>
+                        ) : (
+                          <p>画像をアップロード</p>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                  <div className="recipeInput_item">
+                    <h3 className="recipeInput_title">作り方</h3>
+                    <input className='input' type="text" onChange={(e) => setNewDetail(e.target.value)} value={newDetail} />
+                  </div>
+                </div>
+                <button className='button_additionBtn' type="submit">追加する</button>
+              </form>
             </div>
-            <button className='button_additionBtn' type="submit">追加する</button>
-          </form>
-          </div>
           </div>
         </div>
         <div className="btn_container">
